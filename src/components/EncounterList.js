ca9c79188f83e5aed299ce8129ac342608c9170a
mar-zim <mariuccia.zimmermann@gmx.de>
2020-08-31 22:43:11 +0200

    Implementing basic sorting mechanism

--- src/components/EncounterList.js
+++ src/components/EncounterList.js
@@ -2,10 +2,13 @@ import React from 'react'
 import ListItem from './ListItem'
 
 export default function EncounterList({ encounters }) {
+  const sortedEncounters = encounters.slice().sort((a, b) => b.date - a.date)
+
+  console.log(sortedEncounters)
   return (
     <>
       <h3>People you met</h3>
-      {encounters.map((encounter) => (
+      {sortedEncounters.map((encounter) => (
         <ListItem encounter={encounter} key={encounter.entryId} />
       ))}
     </>
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,10 +1,6 @@
 import React from 'react'
 
 export default function ListItem({ encounter }) {
-  // const day = encounter.date.getDate()
-  // const month = encounter.date.getMonth()
-  // const year = encounter.date.getFullYear()
-
   return (
     <>
       <h4>Date: {encounter.date}</h4>
