06328aed0dff8e002bd7a8ddc9a5b1d6b6ee950e
mar-zim <mariuccia.zimmermann@gmx.de>
2020-08-31 22:33:44 +0200

    Add automatic loading of location

--- src/components/AddPeopleForm.js
+++ src/components/AddPeopleForm.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import 'react-datepicker/dist/react-datepicker.css'
 import { useForm, useFieldArray } from 'react-hook-form'
 import styled from 'styled-components'
@@ -7,6 +7,24 @@ import { v4 as uuidv4 } from 'uuid'
 
 export default function AddPeopleForm({ encounters, setEncounters }) {
   const [userLocation, setUserLocation] = useState('')
+  const [userLocationIsLoading, setUserLocationIsLoading] = useState(true)
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      const lat = position.coords.latitude
+      const lon = position.coords.longitude
+
+      axios
+        .get(
+          `https://eu1.locationiq.com/v1/reverse.php?key=pk.bdf564897f7c87e4a19e06e928604689&lat=${lat}&lon=${lon}&format=json`
+        )
+        .then((response) => response.data)
+        .then((data) => setUserLocation(data.display_name))
+        .catch((error) => console.log(error.message))
+        .finally(setUserLocationIsLoading(false))
+    })
+  }, [])
+
   const inputId = uuidv4()
   const { register, handleSubmit, errors, formState, control, reset } = useForm(
     {
@@ -26,21 +44,6 @@ export default function AddPeopleForm({ encounters, setEncounters }) {
     console.log('Input: ', newEncounter)
     setEncounters([...encounters, newEncounter])
     reset()
-  }
-
-  function getLocation() {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      const lat = position.coords.latitude
-      const lon = position.coords.longitude
-
-      axios
-        .get(
-          `https://eu1.locationiq.com/v1/reverse.php?key=pk.bdf564897f7c87e4a19e06e928604689&lat=${lat}&lon=${lon}&format=json`
-        )
-        .then((response) => response.data)
-        .then((data) => setUserLocation(data.display_name))
-        .catch((error) => console.log(error.message))
-    })
   }
 
   return (
@@ -88,14 +91,18 @@ export default function AddPeopleForm({ encounters, setEncounters }) {
           placeholder="Date"
         />
         {errors.date && <div>Please select a date</div>}
-        <textarea
-          name="location"
-          defaultValue={userLocation}
-          ref={register({
-            required: true,
-          })}
-          placeholder="Press button below to get current location"
-        />
+        {userLocationIsLoading ? (
+          <div>Getting your current location data..</div>
+        ) : (
+          <textarea
+            name="location"
+            defaultValue={userLocation}
+            ref={register({
+              required: true,
+            })}
+            placeholder="Press button below to get current location"
+          />
+        )}
         {errors.location && <div>Please enter a location</div>}
         <input
           name="entryId"
@@ -103,9 +110,9 @@ export default function AddPeopleForm({ encounters, setEncounters }) {
           defaultValue={inputId}
           ref={register()}
         />
-        <button type="button" onClick={getLocation}>
+        {/* <button type="button" onClick={getLocation}>
           Get Location
-        </button>
+        </button> */}
         <button type="submit" disabled={formState.isSubmitting}>
           Submit
         </button>
