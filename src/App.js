860caac7daa8afcd5f2348754d9ec52035b2ca2a
mar-zim <mariuccia.zimmermann@gmx.de>
2020-08-31 10:35:12 +0200

    Add Home.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Home from './pages/Home'
+import styled from 'styled-components'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <StyledMain>
+      <Home />
+    </StyledMain>
+  )
 }
 
-export default App;
+const StyledMain = styled.main`
+  padding: 0 5%;
+  overflow-y: scroll;
+`
