f152e7b9e417f3bc9944b34033cc7a904d095b0e
mar-zim <mariuccia.zimmermann@gmx.de>
2020-10-08 12:21:36 +0200

    Remove comments

--- src/components/AddPeopleForm.js
+++ src/components/AddPeopleForm.js
@@ -28,7 +28,6 @@ export default function AddPeopleForm({ encounters, setEncounters }) {
   })
 
   function onSubmit(newEncounter) {
-    console.log(newEncounter)
     setEncounters([...encounters, newEncounter])
     reset()
     history.push('/')
